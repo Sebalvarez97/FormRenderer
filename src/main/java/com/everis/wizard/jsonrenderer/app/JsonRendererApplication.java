ac15c37926873d9a73b5c3e3224e114e4ad60102
Gabriel Ezequiel Godoy <gabgodoy@everis.com>
2019-02-18 11:25:08 -0300

    COMIT

--- src/main/java/com/everis/wizard/jsonrenderer/app/JsonRendererApplication.java
+++ src/main/java/com/everis/wizard/jsonrenderer/app/JsonRendererApplication.java
@@ -1,13 +1,19 @@
 package com.everis.wizard.jsonrenderer.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JsonRendererApplication {
+	private static final Logger logger= LogManager.getLogger(JsonRendererApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonRendererApplication.class, args);
+		logger.info("mensaje de INFORMACION");
+		logger.error("mensaje de ERROR");
+		logger.debug("Mensaje de debug");
 	}
 
 }
